3c5599904dfbf47100dcc39e0a5d340309e8dc69
Tomas <tomicampos2233@gmail.com>
2023-11-09 16:23:03 -0300

    first commit

---
+++ forms/contact.php
@@ -0,0 +1,36 @@
+<?php
+
+
+ //Configurar correo al cual va a recibir el contacto de losa clientes
+  $receiving_email_address = 'tomicampos2233@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Descomentar en caso de que se vaya a utilizar servidor de correos SMTP
+  /*
+  $contact->smtp = array(
+    'host' => 'localhost',
+    'username' => 'test',
+    'password' => 'test',
+    'port' => '587'
+  );
+*/
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
