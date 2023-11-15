dce3cd0525505cc766af39535eab98fc56191b73
Tomas <tomicampos2233@gmail.com>
2023-11-14 21:03:07 -0300

    contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,49 +1,51 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
-
+// Incluir la biblioteca PHPMailer
+require './PHPMailer/Exception.php';
+require './PHPMailer/PHPMailer.php';
+require './PHPMailer/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $receiving_email_address = 'contacto@degliasoluciones.com'; 
+    // Configuración del servidor de correo
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'contacto@degliasoluciones.com'; // Cambiar a tu dirección de correo
+    $mail->Password = 'TostadaconManteca.8'; // Cambiar a tu contraseña
+    $mail->SMTPSecure = 'ssl'; // Puedes cambiar a 'ssl' si es necesario
+    $mail->Port = 465; // Puedes cambiar el puerto según la configuración de tu servidor
 
+    // Destinatario del correo
+    $receiving_email_address = 'contacto@degliasoluciones.com';
+
+    // Recuperar datos del formulario
     $name = test_input($_POST['name']);
     $email = test_input($_POST['email']);
     $subject = test_input($_POST['subject']);
     $message = test_input($_POST['message']);
 
-    $mail = new PHPMailer(true);
+    // Configuración del correo
+    $mail->setFrom($email, $name);
+    $mail->addAddress($receiving_email_address);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
 
     try {
-        // Configuración del servidor SMTP (Gmail)
-        $mail->SMTPDebug = 0;
-        $mail->isSMTP();
-        $mail->Host = 'smtp.hostinger.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'contacto@degliasoluciones.com'; 
-        $mail->Password = 'TostadaconManteca.8'; 
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
-
-        // Configuración del correo
-        $mail->setFrom($email, $name);
-        $mail->addAddress($receiving_email_address);
-        $mail->Subject = $subject;
-        $mail->Body = $message;
-
         // Enviar el correo
         $mail->send();
-
-        echo 'Enviado con éxito. Gracias por contactarte con nosotros!';
+        echo 'OK';
     } catch (Exception $e) {
-        echo 'Error';
+        echo 'Error: ' . $mail->ErrorInfo;
     }
 } else {
-    echo 'Acceso no permitido.';
+    echo 'No enviado, reintentar.';
 }
 
 function test_input($data) {
@@ -52,4 +54,5 @@ function test_input($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
+
 ?>
