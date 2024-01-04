3f42cfc9b853f4a4dd60a8d1119f9a9739ce69b9
Tomas <tomicampos2233@gmail.com>
2024-01-04 13:11:37 -0300

    Se corrige el color de los botones

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,91 +34,102 @@
   }
 
   /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+ * Navbar links active state on scroll
+ */
+let navbarlinks = select('#navbar .scrollto', true)
 
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
+const navbarlinksActive = () => {
+  let position = window.scrollY + 200
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return
+    let section = select(navbarlink.hash)
+    if (!section) return
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlinks.forEach(link => link.classList.remove('active')); // Remueve 'active' de todos los enlaces
+      navbarlink.classList.add('active'); // Agrega 'active' solo al enlace actual
     }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
   })
+}
 
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
+
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+  let header = select('#header')
+  let offset = header.offsetHeight
+
+  let elementPos = select(el).offsetTop
+  window.scrollTo({
+    top: elementPos - offset,
+    behavior: 'smooth'
+  })
+}
+
+/**
+ * Back to top button
+ */
+let backtotop = select('.back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
     }
-  }, true)
+  }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
+}
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+/**
+ * Mobile nav toggle
+ */
+on('click', '.mobile-nav-toggle', function (e) {
+  select('#navbar').classList.toggle('navbar-mobile')
+  this.classList.toggle('bi-list')
+  this.classList.toggle('bi-x')
+})
 
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
+/**
+ * Mobile nav dropdowns activate
+ */
+on('click', '.navbar .dropdown > a', function (e) {
+  if (select('#navbar').classList.contains('navbar-mobile')) {
+    e.preventDefault()
+    this.nextElementSibling.classList.toggle('dropdown-active')
+  }
+}, true)
+
+/**
+ * Scroll with offset on links with a class name .scrollto
+ */
+on('click', '.scrollto', function (e) {
+  if (select(this.hash)) {
+    e.preventDefault()
+
+    // Remove 'active' class from all navbar links
+    navbarlinks.forEach(navbarlink => {
+      navbarlink.classList.remove('active');
+    });
+
+    let navbar = select('#navbar');
+    if (navbar.classList.contains('navbar-mobile')) {
+      navbar.classList.remove('navbar-mobile')
+      let navbarToggle = select('.mobile-nav-toggle')
+      navbarToggle.classList.toggle('bi-list')
+      navbarToggle.classList.toggle('bi-x')
     }
-  }, true)
+
+    // Add 'active' class to the clicked navbar link
+    this.classList.add('active');
+
+    scrollto(this.hash);
+  }
+}, true)
 
   /**
    * Scroll with ofset on page load with hash links in the url
